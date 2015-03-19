965aa8275f824b298d35d4657b966b86410904bb
Korotaev Boris <korotaev@multivi.ru>
2015-03-19 13:49:40 +0600

    added node caches for entities

--- src/main/java/neon/core/Entity.java
+++ src/main/java/neon/core/Entity.java
@@ -2,229 +2,236 @@ package neon.core;
 
 import neon.util.Bag;
 import neon.util.ImmutableList;
+import neon.util.NodeCache;
 
 import java.util.*;
 
 public class Entity {
 
-    long id;
-    boolean scheduledForRemoval;
-    Engine.ComponentOperationHandler componentOperationHandler;
+	long id;
+	boolean scheduledForRemoval;
+	NodeCache nodeCache;
+	Engine.ComponentOperationHandler componentOperationHandler;
 
-    private List<ComponentListener> listeners;
-    private Map<Class<? extends Component>, List<ComponentListener>> listenersByComponent;
+	private final List<ComponentListener> listeners;
+	private final Map<Class<? extends Component>, List<ComponentListener>> listenersByComponent;
 
-    private Bag<Component> components;
-    private List<Component> componentsArray;
-    private ImmutableList<Component> immutableComponentsArray;
-    private BitSet componentBits;
-    private BitSet aspectBits;
-    private BitSet nodeBits;
+	private final Bag<Component> components;
+	private final List<Component> componentsArray;
+	private final ImmutableList<Component> immutableComponentsArray;
+	private final BitSet componentBits;
+	private final BitSet aspectBits;
+	private final BitSet nodeBits;
 
-    public Entity() {
-        listeners = new ArrayList<>(16);
-        listenersByComponent = new HashMap<>();
+	public Entity() {
+		listeners = new ArrayList<>(16);
+		listenersByComponent = new HashMap<>();
 
-        components = new Bag<>();
-        componentsArray = new ArrayList<>(16);
-        immutableComponentsArray = new ImmutableList<>(componentsArray);
-        componentBits = new BitSet();
-        aspectBits = new BitSet();
-        nodeBits = new BitSet();
-    }
+		components = new Bag<>();
+		componentsArray = new ArrayList<>(16);
+		immutableComponentsArray = new ImmutableList<>(componentsArray);
+		componentBits = new BitSet();
+		aspectBits = new BitSet();
+		nodeBits = new BitSet();
+	}
 
-    public long getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public void addComponentListener(ComponentListener listener) {
-        listeners.add(listener);
-    }
+	public void addComponentListener(ComponentListener listener) {
+		listeners.add(listener);
+	}
 
-    public void addComponentListener(Class<? extends Component> componentClass, ComponentListener listener) {
-        List<ComponentListener> listeners = listenersByComponent.get(componentClass);
+	public void addComponentListener(Class<? extends Component> componentClass, ComponentListener listener) {
+		List<ComponentListener> listeners = listenersByComponent.get(componentClass);
 
-        if (listeners == null) {
-            listeners = new ArrayList<>(16);
-            listenersByComponent.put(componentClass, listeners);
-        }
+		if (listeners == null) {
+			listeners = new ArrayList<>(16);
+			listenersByComponent.put(componentClass, listeners);
+		}
 
-        listeners.add(listener);
-    }
+		listeners.add(listener);
+	}
 
-    public void removeComponentListener(ComponentListener listener) {
-        listeners.remove(listener);
+	public void removeComponentListener(ComponentListener listener) {
+		listeners.remove(listener);
 
-        for (List<ComponentListener> listeners : listenersByComponent.values()) {
-            listeners.remove(listener);
-        }
-    }
+		for (List<ComponentListener> listeners : listenersByComponent.values()) {
+			listeners.remove(listener);
+		}
+	}
 
-    public Entity addComponent(Component component) {
-        if (componentOperationHandler != null) {
-            componentOperationHandler.add(this, component);
-        } else {
-            addInternal(component);
-        }
-        return this;
-    }
+	public Entity addComponent(Component component) {
+		if (componentOperationHandler != null) {
+			componentOperationHandler.add(this, component);
+		} else {
+			addInternal(component);
+		}
+		return this;
+	}
 
-    public Component removeComponent(Class<? extends Component> componentClass) {
-        ComponentType componentType = ComponentType.getFor(componentClass);
-        int componentTypeIndex = componentType.getIndex();
-        Component removeComponent = components.get(componentTypeIndex);
+	public Component removeComponent(Class<? extends Component> componentClass) {
+		ComponentType componentType = ComponentType.getFor(componentClass);
+		int componentTypeIndex = componentType.getIndex();
+		Component removeComponent = components.get(componentTypeIndex);
 
-        if (componentOperationHandler != null) {
-            componentOperationHandler.remove(this, componentClass);
-        } else {
-            removeInternal(componentClass);
-        }
+		if (componentOperationHandler != null) {
+			componentOperationHandler.remove(this, componentClass);
+		} else {
+			removeInternal(componentClass);
+		}
 
-        return removeComponent;
-    }
+		return removeComponent;
+	}
 
-    public void removeAllComponents() {
-        if (componentOperationHandler != null) {
-            componentOperationHandler.removeAll(this);
-        } else {
-            removeAllInternal();
-        }
-    }
+	public void removeAllComponents() {
+		if (componentOperationHandler != null) {
+			componentOperationHandler.removeAll(this);
+		} else {
+			removeAllInternal();
+		}
+	}
 
-    void removeAllInternal() {
-        while (componentsArray.size() > 0) {
-            removeInternal(componentsArray.get(0).getClass());
-        }
-    }
+	void removeAllInternal() {
+		while (componentsArray.size() > 0) {
+			removeInternal(componentsArray.get(0).getClass());
+		}
+	}
 
-    public ImmutableList<Component> getComponents() {
-        return immutableComponentsArray;
-    }
+	public ImmutableList<Component> getComponents() {
+		return immutableComponentsArray;
+	}
 
-    public <T extends Component> T getComponent(Class<T> componentClass) {
-        return getComponent(ComponentType.getFor(componentClass));
-    }
+	public <T extends Component> T getComponent(Class<T> componentClass) {
+		return getComponent(ComponentType.getFor(componentClass));
+	}
 
-    @SuppressWarnings("unchecked")
-    <T extends Component> T getComponent(ComponentType componentType) {
-        int componentTypeIndex = componentType.getIndex();
+	@SuppressWarnings("unchecked")
+	<T extends Component> T getComponent(ComponentType componentType) {
+		int componentTypeIndex = componentType.getIndex();
 
-        if (componentTypeIndex < components.getCapacity()) {
-            return (T) components.get(componentType.getIndex());
-        } else {
-            return null;
-        }
-    }
+		if (components.isIndexWithinBounds(componentTypeIndex)) {
+			return (T) components.get(componentType.getIndex());
+		} else {
+			return null;
+		}
+	}
 
-    public boolean hasComponent(Class<? extends Component> componentClass) {
-        return hasComponent(ComponentType.getFor(componentClass));
-    }
+	public boolean hasComponent(Class<? extends Component> componentClass) {
+		return hasComponent(ComponentType.getFor(componentClass));
+	}
 
-    boolean hasComponent(ComponentType componentType) {
-        return componentBits.get(componentType.getIndex());
-    }
+	boolean hasComponent(ComponentType componentType) {
+		return componentBits.get(componentType.getIndex());
+	}
 
-    public <T extends Node> T asNode(Class<T> nodeClass) {
-        return NodeFamily.getFor(nodeClass).get(this);
-    }
+	public <T extends Node> T asNode(Class<T> nodeClass) {
+		if (nodeCache == null) {
+			return NodeFamily.getFor(nodeClass).get(this);
+		}
 
-    public boolean matchesNode(Class<? extends Node> nodeClass) {
-        return NodeFamily.getFor(nodeClass).matches(this);
-    }
+		T node = nodeCache.get(nodeClass);
+		return node != null ? node : NodeFamily.getFor(nodeClass).get(this);
+	}
 
-    BitSet getComponentBits() {
-        return componentBits;
-    }
+	public boolean matchesNode(Class<? extends Node> nodeClass) {
+		return NodeFamily.getFor(nodeClass).matches(this);
+	}
 
-    BitSet getAspectBits() {
-        return aspectBits;
-    }
+	BitSet getComponentBits() {
+		return componentBits;
+	}
 
-    BitSet getNodeBits() {
-        return nodeBits;
-    }
+	BitSet getAspectBits() {
+		return aspectBits;
+	}
 
-    Entity addInternal(Component component) {
-        Class<? extends Component> componentClass = component.getClass();
+	BitSet getNodeBits() {
+		return nodeBits;
+	}
 
-        Component oldComponent = getComponent(componentClass);
+	Entity addInternal(Component component) {
+		Class<? extends Component> componentClass = component.getClass();
 
-        if (component == oldComponent) {
-            return this;
-        }
+		Component oldComponent = getComponent(componentClass);
 
-        if (oldComponent != null) {
-            removeInternal(componentClass);
-        }
+		if (component == oldComponent) {
+			return this;
+		}
 
-        int componentTypeIndex = ComponentType.getIndexFor(componentClass);
+		if (oldComponent != null) {
+			removeInternal(componentClass);
+		}
 
-        components.set(componentTypeIndex, component);
-        componentsArray.add(component);
-        componentBits.set(componentTypeIndex);
-        notifyComponentAdded(component);
-        return this;
-    }
+		int componentTypeIndex = ComponentType.getIndexFor(componentClass);
 
-    Component removeInternal(Class<? extends Component> componentClass) {
-        ComponentType componentType = ComponentType.getFor(componentClass);
-        int componentTypeIndex = componentType.getIndex();
-        Component removeComponent = components.get(componentTypeIndex);
+		components.set(componentTypeIndex, component);
+		componentsArray.add(component);
+		componentBits.set(componentTypeIndex);
+		notifyComponentAdded(component);
+		return this;
+	}
 
-        if (removeComponent != null) {
-            components.set(componentTypeIndex, null);
-            componentsArray.remove(removeComponent);
-            componentBits.clear(componentTypeIndex);
-            notifyComponentRemoved(removeComponent);
-        }
+	Component removeInternal(Class<? extends Component> componentClass) {
+		ComponentType componentType = ComponentType.getFor(componentClass);
+		int componentTypeIndex = componentType.getIndex();
+		Component removeComponent = components.get(componentTypeIndex);
 
-        return removeComponent;
-    }
+		if (removeComponent != null) {
+			components.remove(componentTypeIndex);
+			componentsArray.remove(removeComponent);
+			componentBits.clear(componentTypeIndex);
+			notifyComponentRemoved(removeComponent);
+		}
 
-    private void notifyComponentAdded(Component component) {
-        for (ComponentListener listener : listeners) {
-            listener.componentAdded(this, component);
-        }
+		return removeComponent;
+	}
 
-        List<ComponentListener> listeners = listenersByComponent.get(component.getClass());
-        if (listeners != null) {
-            for (ComponentListener listener : listeners) {
-                listener.componentAdded(this, component);
-            }
-        }
-    }
+	private void notifyComponentAdded(Component component) {
+		for (ComponentListener listener : listeners) {
+			listener.componentAdded(this, component);
+		}
 
-    private void notifyComponentRemoved(Component component) {
-        for (ComponentListener listener : listeners) {
-            listener.componentRemoved(this, component);
-        }
+		List<ComponentListener> listeners = listenersByComponent.get(component.getClass());
+		if (listeners != null) {
+			for (ComponentListener listener : listeners) {
+				listener.componentAdded(this, component);
+			}
+		}
+	}
 
-        List<ComponentListener> listeners = listenersByComponent.get(component.getClass());
-        if (listeners != null) {
-            for (ComponentListener listener : listeners) {
-                listener.componentRemoved(this, component);
-            }
-        }
-    }
+	private void notifyComponentRemoved(Component component) {
+		for (ComponentListener listener : listeners) {
+			listener.componentRemoved(this, component);
+		}
 
-    public boolean isScheduledForRemoval() {
-        return scheduledForRemoval;
-    }
+		List<ComponentListener> listeners = listenersByComponent.get(component.getClass());
+		if (listeners != null) {
+			for (ComponentListener listener : listeners) {
+				listener.componentRemoved(this, component);
+			}
+		}
+	}
 
-    @Override
-    public int hashCode() {
-        return (int) (id ^ (id >>> 32));
-    }
+	public boolean isScheduledForRemoval() {
+		return scheduledForRemoval;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof Entity)) {
-            return false;
-        }
-        Entity other = (Entity) obj;
-        return id == other.id;
-    }
+	@Override
+	public int hashCode() {
+		return (int) (id ^ (id >>> 32));
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Entity)) {
+			return false;
+		}
+		Entity other = (Entity) obj;
+		return id == other.id;
+	}
 }
---
+++ src/main/java/neon/util/NodeCache.java
@@ -0,0 +1,28 @@
+package neon.util;
+
+import neon.core.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class NodeCache {
+
+	private final Map<Class<? extends Node>, Node> nodeCache;
+
+	public NodeCache() {
+		nodeCache = new HashMap<>();
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Node> T get(Class<T> nodeClass) {
+		return (T) nodeCache.get(nodeClass);
+	}
+
+	public <T extends Node> void put(T node) {
+		nodeCache.put(node.getClass(), node);
+	}
+
+	public <T extends Node> void remove(Class<T> nodeClass) {
+		nodeCache.remove(nodeClass);
+	}
+}
